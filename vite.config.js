4df40a8759cfa63a2c88979862651f504d6d696c
이재진 <starcx@naver.com>
2026-05-09 11:21:27 +0900

    AWS 반영

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,28 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  css: {
+    transformer: "postcss", // ← lightningcss 대신 postcss 사용
+  },
+  build: {
+    cssMinify: false,
+  },
   server: {
     port: 7000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
